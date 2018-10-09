43cae4eb40fb46533bfd07e1b4043429ddb6bc12
Vladimir Belov <belov@itep.ru>
2018-10-10 00:11:17 +0300

    Fix build of derived projets.

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ filename=`readlink -f "${BASH_SOURCE[0]}"`
 basedir=`dirname "${filename}"`
 
 export CMTPROJECTPATH="$basedir/share${CMTPROJECTPATH:+:${CMTPROJECTPATH}}"
-export PYTHONPATH="$basedir/python:$workdir/lib${PYTHONPATH:+:${PYTHONPATH}}"
+export PYTHONPATH="$basedir/python:$basedir/lib${PYTHONPATH:+:${PYTHONPATH}}"
 export LD_LIBRARY_PATH="$basedir/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
